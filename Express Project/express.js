232fe06f25296e13cf912d2f4bcf77b4621215e2
ichater <chaterizaak@gmail.com>
2019-09-21 16:26:36 +1000

    delete

--- Express Project/express.js
+++ Express Project/express.js
@@ -5,12 +5,14 @@ const express = require("express"),
  	  path = require("path"),
  	  routes = require("router"),
  	  layout= require("express-layout"),
- 	  mongoose = require("mongoose");
+	   mongoose = require("mongoose"),
+	   methodOveride = require("method-override");
 
 mongoose.connect("mongodb://localhost/brunoToDo", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(methodOveride("_method"));
 
 //Schema setup
 let brunoSchema = new mongoose.Schema({
@@ -46,18 +48,18 @@ app.get("/survey", function(req, res){
 
 app.get("/toDo", function(req, res){
 	//Get all toDos from db
-	brunoList.find({}, function (err, brunoList){
+	brunoList.find({}, function (err, brunoList1){
 		if(err){
 			console.log(err);
 		} else {
-			res.render("ToDo.ejs", {brunoList: brunoList});
+			res.render("ToDo.ejs", {brunoList: brunoList1});
 		}
 	});
 });
 
 app.post("/addToDo", function(req, res){
-	let listItem = req.body.listItem;
-	let toDo1 = {brunoList: brunoList}
+	let newToDo = req.body.newToDo;
+	let toDo1 = {listItem: newToDo}
 	//create new ToDo and save to database
 	brunoList.create(toDo1, function(err, newtodo1){
 		if(err){
@@ -69,16 +71,21 @@ app.post("/addToDo", function(req, res){
 	console.log(req.body.newToDo);
 });
 
-// app.post("/removeToDo", function(req, res){
-// 	let rtoDo = req.body.rtoDo;
-// 	for(let i = 0; i < brunoList.length; i++){ 
-// 		if (brunoList[i] === req.body.rtoDo) {
-// 			brunoList.splice(i, 1); 
-// 		}
-// 	 };
-// 	 res.redirect("/toDo")
-// 	 console.log("Removed: "  + req.body.removeToDo);
-// });
+app.get("/addToDo/new", function(req, res){
+	res.render("ToDo.ejs");
+});
+
+
+app.delete("/toDo/:id", function(req,res){
+	brunoList.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/toDo");
+			console.log(err);
+		} else {
+			res.redirect("/toDo");
+		}
+	});
+});
 
 
 app.listen(3000, function(){
