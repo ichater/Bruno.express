e0ef5fc9abfbb0d4d8b048720d2e442b4007abc9
ichater <chaterizaak@gmail.com>
2019-09-22 11:23:32 +1000

    survey started

--- Express Project/public/routes.js
+++ Express Project/public/routes.js
@@ -1,7 +1,26 @@
 
 
-
 //Jquery toDo below
+
+
+//default to right answer
+$(".incorrect").on("click", function(){
+    $(".correct").prop( "checked", true );
+    $(".incorrect").prop( "checked", false);
+    alert("correct! Dogs are superior!");
+});
+
+$(".incorrect2").on("click", function(){
+    $(".correct2").prop( "checked", true );
+    $(".incorrect2").prop( "checked", false);
+    alert("correct! It's all of the above <3!");
+});
+
+$(".incorrect3").on("click", function(){
+    $(".correct3").prop( "checked", true );
+    $(".incorrect3").prop( "checked", false);
+    alert("Thats right... 'strictly' 2 borks per day");
+});
 
 //Line Through
 $(".jToDo").on("click", "li", function(){
