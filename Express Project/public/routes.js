73517e2399f04a061273123519bd3ae380542268
ichater <chaterizaak@gmail.com>
2019-09-24 13:05:59 +1000

    CSS

--- Express Project/public/routes.js
+++ Express Project/public/routes.js
@@ -4,23 +4,43 @@
 
 
 //default to right answer
+let qOne = () =>{alert('correct! Dogs are superior!');}  
+let qTwo = () =>{alert("correct! It's all of the above <3!");}  
+
 $(".incorrect").on("click", function(){
     $(".correct").prop( "checked", true );
     $(".incorrect").prop( "checked", false);
-    alert("correct! Dogs are superior!");
+    qOne()   
 });
 
 $(".incorrect2").on("click", function(){
     $(".correct2").prop( "checked", true );
     $(".incorrect2").prop( "checked", false);
-    alert("correct! It's all of the above <3!");
+    qTwo();
 });
 
 $(".incorrect3").on("click", function(){
     $(".correct3").prop( "checked", true );
     $(".incorrect3").prop( "checked", false);
-    alert("Thats right... 'strictly' 2 borks per day");
+    alert("Human are you crazy! It's 2 borks per day max!");
 });
+//when right answer is clicked
+$(".correct").on("click", function(){
+    qOne();
+});
+
+$(".correct2").on("click", function(){
+    qTwo();
+});
+
+$(".correct3").on("click", function(){
+    alert("Thats right... 'strictly' 2 borks per day ;)");
+});
+
+
+
+
+
 
 //Line Through
 $(".jToDo").on("click", "li", function(){
